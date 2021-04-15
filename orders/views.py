73b155de2fbcf6a5e867986da733821d2723fc49
Seojun Youn <92sursumcorda@gmail.com>
2021-04-15 21:17:28 +0900

    test

--- orders/views.py
+++ orders/views.py
@@ -4,22 +4,22 @@ from django.http.response import JsonResponse
 from django.views         import View
 
 from .models         import Order, Status, ProductOrder
-# from products.models import Product
+from products.models import Product
 
 # @loginDecorator
 class AddCartView(View):
     def post(self, request):
         
-        # user = request.user
+        user = request.user
         try:
             data = json.loads(request.body)
             
-            # if not Order.objects.filter(user_id = request.user).exist:
-            #     Order.objects.create(status=Status.objects.get(id=2),
-            #                          user = User.objects.get(id=request.user)
-            #                          )
-            # if Order.objects.get(user_id = request.user).status_id != 3:
-            #     None
+            if not Order.objects.filter(user_id = request.user).exist:
+                Order.objects.create(status=Status.objects.get(id=2),
+                                     user = User.objects.get(id=request.user)
+                                     )
+            if Order.objects.get(user_id = request.user).status_id != 3:
+                None
 
             
             
--- products/models.py
+++ products/models.py
@@ -20,17 +20,17 @@ class SubCategory(models.Model):
         db_table = 'sub_categories'
         
 class Product(models.Model):
-    name           = models.CharField(max_length = 100)
-    price          = models.DecimalField(max_digits = 10, decimal_places = 2)
-    discount_rate  = models.DecimalField(max_digits = 5, decimal_places = 2, default = 0)
-    unit           = models.DecimalField(max_digits = 5, decimal_places = 2)
-    best_before    = models.IntegerField()
-    sub_category   = models.ForeignKey('SubCategory', on_delete = SET_NULL, null = True)
-    created_at     = models.DateTimeField(auto_now_add = True)
-    updated_at     = models.DateTimeField(auto_now = True)
+    name          = models.CharField(max_length = 100)
+    price         = models.DecimalField(max_digits = 10, decimal_places = 2)
+    discount_rate = models.DecimalField(max_digits = 5, decimal_places = 2, default = 0)
+    unit          = models.DecimalField(max_digits = 5, decimal_places = 2)
+    best_before   = models.IntegerField()
+    sub_category  = models.ForeignKey('SubCategory', on_delete = SET_NULL, null = True)
+    created_at    = models.DateTimeField(auto_now_add = True)
+    updated_at    = models.DateTimeField(auto_now = True)
     
     def real_price(self):
-        return self.price*(1 - self.discount_rate)    
+        return self.price * (1 - self.discount_rate)    
     
     class Meta:
         db_table = "products"
