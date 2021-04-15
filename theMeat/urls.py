e6168d339a41ae66ee3c5c5a57b55f510ca35f34
Seojun Youn <92sursumcorda@gmail.com>
2021-04-15 13:11:35 +0900

    models: 주석 제거, 공백 제거

--- theMeat/urls.py
+++ theMeat/urls.py
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 from django.urls import path, include
 
 urlpatterns = [
     path('products', include("products.urls")),
     path('users', include("users.urls")),
     path('orders', include("orders.urls")),
-=======
-from django.urls import path
-
-urlpatterns = [
-    
->>>>>>> origin/main
 ]
