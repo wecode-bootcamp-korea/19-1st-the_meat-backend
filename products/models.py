9874cec57b9d15a6086acc34e007670fb0970857
Seojun Youn <92sursumcorda@gmail.com>
2021-04-17 17:25:18 +0900

    Product.unit column delete

--- products/models.py
+++ products/models.py
@@ -25,7 +25,6 @@ class Product(models.Model):
     name           = models.CharField(max_length = 100)
     original_price = models.DecimalField(max_digits = 12, decimal_places = 2)
     discount_rate  = models.DecimalField(max_digits = 5, decimal_places = 2, default = 0)
-    unit           = models.DecimalField(max_digits = 5, decimal_places = 2)
     best_before    = models.IntegerField()
     sub_category   = models.ForeignKey('SubCategory', on_delete = SET_NULL, null = True)
     created_at     = models.DateTimeField(auto_now_add = True)
