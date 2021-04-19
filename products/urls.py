7c96267ad9aba032cef5ab14bcca2499408bcbd7
jeewonn <jeewonn0504@gmail.com>
2021-04-19 13:32:55 +0900

    category,subcategory,saleproduct,mdpick,category of product

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import ProductView
+from .views import CategorypageView,SubCategoryView,CategoryView,SaleproductView,MdpickView
 
 urlpatterns = [
-    
+    path('/category/<int:subcategory_id>',CategorypageView.as_view()),
+    path('/subcategory', SubCategoryView.as_view()),
+    path('/category', CategoryView.as_view()),
+    path('/sale', SaleproductView.as_view()),
+    path('/mdpick/<int:category_id>', MdpickView.as_view())
 ]
