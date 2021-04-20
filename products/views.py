5d49c220b7d91ecbd588e39a6dc59d7667c232f8
jeewonn <jeewonn0504@gmail.com>
2021-04-20 18:55:10 +0900

    Modify: products/views.py 수정 - Q 객체 사용 - discount_rate int로 front에게 전송된것 확인

--- products/views.py
+++ products/views.py
@@ -1,26 +1,29 @@
 from django.http      import JsonResponse
 from django.views     import View
 
-from products.models import Product
+from products.models  import Product
+from django.db.models import Q
 
 class ProductListView(View):
     def get(self, request):
         category = request.GET.get('category', None)
         sub_category = request.GET.get('sub_category', None)
+        pick = request.GET.get('pick', None)
         discount = request.GET.get('discount', None)
         new = request.GET.get('new', None)
-        pick = request.GET.get('pick', None)
 
-        products = Product.objects.all()
+        q = Q()
 
         if category:
-            products = products.filter(sub_category__category__name=category)
+            q &= Q(sub_category__category__name=category)
 
         if sub_category:
-            products = products.filter(sub_category__name=sub_category)
+            q &= Q(sub_category__name=sub_category)
 
         if pick:
-            products = products.filter(sub_category__category__name=pick)[:4]
+            q &= Q(sub_category__category__name=pick)
+
+        products = Product.objects.filter(q)
 
         if discount:
             products = products.order_by('-discount_rate')[:6]
@@ -35,9 +38,9 @@ class ProductListView(View):
                     'created_at'   : product.created_at,
                     'name'         : product.name,
                     'image_url'    : [product_image.image_url for product_image in product.productimage_set.all()],
-                    'price'        : product.get_real_price()['real_price'],
+                    'price'        : int(product.get_real_price()['real_price']),
                     'real_price'   : int(product.original_price),
-                    'discount_rate': int(float(product.discount_rate)),
+                    'discount_rate': int(product.discount_rate),
         } for product in products]
 
         return JsonResponse({'result': result}, status=200)
