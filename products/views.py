c8a606ecbceebde9a80a04732511fdd13ab43831
jeewonn <jeewonn0504@gmail.com>
2021-04-20 10:18:02 +0900

    Add: product filter 생성 - category, sub_category - 할인율, 신상품

--- products/views.py
+++ products/views.py
@@ -1,7 +1,37 @@
-import json
+from django.http      import JsonResponse
+from django.views     import View
 
-from django.http.response import JsonResponse
-from django.views         import View
+from products.models import *
 
-class ProductView(View):
-    pass
+class ProductListView(View):
+    def get(self, request):
+        category = request.GET.get('category', None)
+        sub_category = request.GET.get('sub_category', None)
+        discount = request.GET.get('sort', None)
+        new = request.GET.get('new', None)
+
+        products = Product.objects.all()
+
+        if category:
+            products = products.filter(sub_category__category__name=category)
+
+        if sub_category:
+            products = products.filter(sub_category__name=sub_category)
+
+        if discount:
+            products = products.order_by('-discount_rate')[:6]
+
+        if new:
+            products = products.order_by('-created_at')[:4]
+
+        result = [{
+                    'id'           : product.id,
+                    'created_at'   : product.created_at,
+                    'name'         : product.name,
+                    'image_url'    : [product_image.image_url for product_image in product.productimage_set.all()],
+                    'price'        : int(float(product.get_real_price()['real_price'])),
+                    'real_price'   : int(float(product.original_price)),
+                    'discount_rate': int(float(product.discount_rate)),
+        } for product in products]
+
+        return JsonResponse({'result': result}, status=200)
