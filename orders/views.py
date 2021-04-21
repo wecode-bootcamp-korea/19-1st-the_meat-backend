7b1ab8acf55f63e6ea18007c92f97d794fe864bd
Seojun Youn <92sursumcorda@gmail.com>
2021-04-21 14:06:23 +0900

    cart view 수정 요청 반영. order 객체에서 product 참조 및 cart table로 역참조하는 flow

--- orders/views.py
+++ orders/views.py
@@ -9,89 +9,88 @@ from .models         import Order, Status, ProductOrder
 from users.models    import User
 from products.models import Product
 
-# @loginDecorator
 class CartView(View):
+    # @login_decorator
     def post(self, request):
         try:
-            # user_id    = request.user
-            user_id    = 1
-            data       = json.loads(request.body)
-            user_order = Order.objects.filter(Q(user_id = user_id) & Q(status_id = 1)).first()
-
-            if user_order.productorder_set.filter(product_id=data['id']).exists():
-                cart_instance = user_order.productorder_set.get(product_id = data['id'])
-                cart_instance.quantity += int(data['quantity'])
-                cart_instance.save()
+            data                      = json.loads(request.body)
+            user_id                   = request.user
+            user_order, order_created = Order.objects.get_or_create(user_id = user_id, status_id = 1)
+            
+            if user_order.productorder_set.filter(product_id = data['id']).exists():
+                user_cart           = user_order.productorder_set.get(product_id = data['id'])
+                user_cart.quantity += int(data['quantity'])
+                user_cart.save()
             else:
-                ProductOrder.objects.create(order = user_order,
-                                            product_id = data['id'],
-                                            quantity = data['quantity'])
-                
-            result_object = user_order.productorder_set.get(product_id = data['id'])
+                user_cart = user_order.productorder_set.create(order      = user_order,
+                                                               product_id = data['id'],
+                                                               quantity   = data['quantity'])
             result        = {
-                'id'       : result_object.product_id,
-                'order_id' : result_object.order_id,
-                'quantity' : result_object.quantity,
+                'id'       : user_cart.product_id,
+                'order_id' : user_cart.order_id,
+                'quantity' : user_cart.quantity,
             } 
             return JsonResponse({'result': result}, status = 201)
     
-        except KeyError as e:
-            return JsonResponse({'message': "Invalid KeyError by '{e}'"}, status = 400)
+        except Order.MultipleObjectsReturned:
+            return JsonResponse({'Error': "MORE"}, status = 400)
     
+    # @login_decorator
     def get(self, request):
         try:
-            user_id        = request.user
-            in_cart_products = ProductOrder.objects.filter(
-                order = Order.objects.filter(Q(user_id = user_id) &
-                                             Q(status_id = 1)).first())
+            user_id = request.user
             
-            results = [{
-                'id'         : in_cart_product.product.id,
-                'name'       : in_cart_product.product.name,
-                'image_url'  : [x.image_url for x in in_cart_product.product.productimage_set.all()],
-                'real_price' : in_cart_product.product.get_real_price()['real_price'],
-                'total_price': in_cart_product.get_total_price()}
-                           for in_cart_product in in_cart_products]
-
+            user_order, order_created = Order.objects.get_or_create(user_id   = user_id,
+                                                                    status_id = 1
+                                                                    )
+            if not order_created:
+                products_in_cart = user_order.product.all()
+                results = [{
+                    'id'         : product.id,
+                    'name'       : product.name,
+                    'image_url'  : [image.image_url for image in product.productimage_set.all()],
+                    'real_price' : product.get_real_price()['real_price'],}
+                    for product in products_in_cart]
+            else: results = []
+            
             return JsonResponse({'result': results}, status = 200)
         
-        except KeyError as e:
-            return JsonResponse({'result': f'Invalid KeyError by {e}'}, status = 400)
+        except Order.MultipleObjectsReturned:
+            return JsonResponse({'Error': "MORE"}, status = 400)
         
+    # @login_decorator
     def patch(self, request):
         try:
-            data = json.loads(request.body)
-            user_id        = request.user
-            
-            in_cart_product = ProductOrder.objects.get(Q(product_id = data['id']) &
-                                                       Q(order      = Order.objects.get(Q(user_id = user_id) &
-                                                                                      Q(status_id = 1))))
-            in_cart_product.quantity = int(data['quantity'])
-            in_cart_product.save()
+            data                      = json.loads(request.body)
+            user_id                   = data['user_id']
+            user_order, order_created = Order.objects.get_or_create(user_id   = user_id,
+                                                                    status_id = 1)
+            if not order_created:
+                product_in_cart          = user_order.productorder_set.get(product_id = data['id'])
+                product_in_cart.quantity = int(data['quantity'])
+                product_in_cart.save()
     
-            results = {'id'         : data['id'],
-                       'quantity'   : in_cart_product.quantity,
-                       'total_price': in_cart_product.get_total_price()}
+                results   = [{'id'       : data['id'],
+                             'quantity' : product_in_cart.quantity}]
+            else:
+                results = []
             
-            return JsonResponse({'result': results}, status = 200)
+            return JsonResponse({'result': "results"}, status = 200)
         
-        except KeyError as e:
-            return JsonResponse({'result': f'Invalid KeyError by {e}'}, status = 400)
+        except Order.MultipleObjectsReturned:
+            return JsonResponse({'message': "MORE"}, status = 400)
     
+    # @login_decorator
     def put(self, request):
         try:
-            user_id = request.user
             data = json.loads(request.body)
-
-            ProductOrder.objects.get(Q(product_id = data['id']) &
-                                     Q(order      = Order.objects.get(Q(user_id = user_id) &
-                                                                      Q(status_id = 1)))).delete()
+            user_id = data.get("user_id")
             
-            return JsonResponse({'message': 'Success'}, status = 200)
-        
-        except KeyError as e:
-            return JsonResponse({'message': f'Invalid KeyError by {e}'}, status = 400)
+            user_order, order_created = Order.objects.get_or_create(user_id   = user_id,
+                                                                    status_id = 1)
+            user_cart = user_order.productorder_set.get(product_id = data['id']).delete()
 
-class OrderView(View):
-    def post(self, request):
-        pass
+            return JsonResponse({'message': results}, status = 200)
+        
+        except Order.MultipleObjectsReturned:
+            return JsonResponse({'Error': "MORE"}, status = 400)
