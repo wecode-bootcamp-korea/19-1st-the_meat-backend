701f8c86148a9b8d07d5172971097bacb9a32483
jeewonn <jeewonn0504@gmail.com>
2021-04-22 19:57:05 +0900

    pick 수정

--- products/views.py
+++ products/views.py
@@ -21,10 +21,10 @@ class ProductListView(View):
         if sub_category:
             q &= Q(sub_category__name=sub_category)
 
-        if pick:
-            q &= Q(sub_category__category__name=pick)
-
         products = Product.objects.filter(q)
+
+        if pick:
+            products = products.filter(sub_category__category__name=pick)[:4]
 
         if discount:
             products = products.order_by('-discount_rate')[:6]
